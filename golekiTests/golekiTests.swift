21710496c0216dadcc687ac48e6009fedfab8ca2
Mika Suryofakhri Rahwono <mikasuryof@outlook.com>
2024-06-11 15:52:59 +0700

    Initial Commit

---
+++ golekiTests/golekiTests.swift
@@ -0,0 +1,17 @@
+//
+//  golekiTests.swift
+//  golekiTests
+//
+//  Created by Mika S Rahwono on 11/06/24.
+//
+
+import Testing
+@testable import goleki
+
+struct golekiTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
