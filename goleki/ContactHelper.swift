843a674e7c37067bf0b336e33714d9de02a898dd
Mika Suryofakhri Rahwono <mikasuryof@outlook.com>
2024-06-19 15:06:47 +0700

    Adding movement logic

---
+++ goleki/ContactHelper.swift
@@ -0,0 +1,33 @@
+//
+//  ContactHelper.swift
+//  goleki
+//
+//  Created by Mika S Rahwono on 19/06/24.
+//
+
+import SpriteKit
+
+enum bitMask: UInt32 {
+    case person = 0x1
+    case floor = 0x5
+    case highlight = 0x3
+    case walls = 0x2
+    case raycast = 0x4
+}
+
+extension ResidenceScene: SKPhysicsContactDelegate {
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let bitmaskA = contact.bodyA.categoryBitMask
+        let bitmaskB = contact.bodyB.categoryBitMask
+        
+        if (bitmaskA == bitMask.raycast.rawValue && bitmaskB == bitMask.floor.rawValue && !isTouchEnded) {
+            highlight.position = contact.bodyB.node!.position
+            isTouchEnded = false
+            
+        } else if (bitmaskA == bitMask.floor.rawValue && bitmaskB == bitMask.raycast.rawValue && !isTouchEnded) {
+            highlight.position = contact.bodyA.node!.position
+            isTouchEnded = false
+        }
+    }
+}
--- goleki/GameViewController.swift
+++ goleki/GameViewController.swift
@@ -8,48 +8,46 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import AVFAudio
 
 class GameViewController: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let scene = GameScene(fileNamed: "GameScene")
+        scene!.size = view.bounds.size
+        scene!.scaleMode = .aspectFill
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
-        }
+        let skView = view as! SKView
+        skView.ignoresSiblingOrder = true
+        skView.presentScene(scene)
+//        skView.showsPhysics = true
+        
+//        playSaveSound()
     }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
+    
+//    func playSaveSound(){
+//        let path = Bundle.main.path(forResource: "backgroundMusic.mp3", ofType: nil)!
+//        let url = URL(fileURLWithPath: path)
+//
+//        do {
+//            audioPlayer = try AVAudioPlayer(contentsOf: url)
+//            audioPlayer.prepareToPlay()
+//            audioPlayer.numberOfLoops = -1
+//            audioPlayer.play()
+//            audioPlayer.volume = 0.3
+//        } catch {
+//            print("couldn't load the file")
+//        }
+//    }
 
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+        }
 }
